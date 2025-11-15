318e56f5fee42376d2f16893d9b38c953ce7576c
meidinarevisp <revimeidina72@gmail.com>
2025-11-15 19:08:56 +0700

    update

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -88,14 +88,11 @@ function PokemonList() {
 
   if (loading) {
     return (
-      <div className="flex flex-col justify-center items-center h-96 gap-4">
+      <div className="flex justify-center items-center h-96">
         <div className="relative w-20 h-20">
           <div className="absolute inset-0 border-4 border-white/30 rounded-full"></div>
           <div className="absolute inset-0 border-4 border-t-white rounded-full animate-spin"></div>
         </div>
-        <p className="text-white font-mono text-sm tracking-widest animate-pulse">
-          LOADING...
-        </p>
       </div>
     );
   }
